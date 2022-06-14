c7520b0c43f3dbff9801d7e71331b4d4b36ca617
AntonellaDiazPico <antonelladiazpico@gmail.com>
2022-06-14 13:51:19 +1000

    initial commit

---
+++ lib/hitcounter.d.ts
@@ -0,0 +1,22 @@
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+export interface HitCounterProps {
+    /** the function for which we want to count url hits **/
+    downstream: lambda.IFunction;
+    /**
+    * The read capacity units for the table
+    *
+    * Must be greater than 5 and lower than 20
+    *
+    * default 5
+    */
+    readCapacity?: number;
+}
+export declare class HitCounter extends Construct {
+    /** allows accessing the counter function */
+    readonly handler: lambda.Function;
+    /** the hit counter table */
+    readonly table: dynamodb.Table;
+    constructor(scope: Construct, id: string, props: HitCounterProps);
+}
